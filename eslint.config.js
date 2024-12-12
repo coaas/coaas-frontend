5794b80422fb1a9240bbbac34eefc0a1b83f57c9
maxim <bozzhik@ya.ru>
2024-12-12 21:19:35 +0300

    chore: добавил правило в `eslint` конфиг для настройки `endOfLine`
    исправляет ошибку `Delete `␍` eslint(prettier/prettier)`

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import tseslint from 'typescript-eslint';
 
@@ -15,7 +15,7 @@ export default tseslint.config(
       js.configs.recommended,
       ...tseslint.configs.recommended,
       eslintConfigPrettier,
-      eslintPluginPrettierRecommended
+      eslintPluginPrettierRecommended,
     ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
@@ -33,6 +33,7 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
     },
   },
 );
