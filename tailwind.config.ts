eb147864cbef33f09bfb6b5670bda253fa222edf
maxim <bozzhik@ya.ru>
2024-12-12 22:05:37 +0300

    feat: интеграция Tailwind CSS и базовая настройка конфигурации

---
+++ tailwind.config.ts
@@ -0,0 +1,36 @@
+import type { Config } from 'tailwindcss';
+
+export default {
+  content: [
+    './src/scenes/components/*.{ts,tsx}',
+    './src/scenes/components/**/**/*.{ts,tsx}',
+  ],
+  theme: {
+    screens: {
+      xl: { max: '1780px' },
+      lg: { max: '1200px' },
+      // md: {max: '1024px'},
+      sm: { max: '500px' },
+    },
+    colors: {
+      background: '#2D2C33',
+      area: {
+        DEFAULT: '#343D5C',
+        dark: '#323548',
+      },
+      stroke: {
+        DEFAULT: '#4E608D',
+        gray: '#43424A',
+      },
+      blue: {
+        DEFAULT: '#507EF5',
+        light: '#7086A6',
+      },
+      white: '#FFFFFF',
+      gray: '#B6B6B6',
+      transparent: 'transparent',
+    },
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config;
