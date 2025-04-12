837e14fe8c032a0cc0f3317f5d2b1c43fdfebb0d
Gleb001 <lichagingleb@mail.ru>
2025-04-12 13:17:22 +0500

    style: change login form styling

--- src/scenes/components/Login/Login.tsx
+++ src/scenes/components/Login/Login.tsx
@@ -39,38 +39,61 @@ export const Login = () => {
   };
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col justify-center dark:bg-background mt-20">
       <form
         onSubmit={e => {
           e.preventDefault();
           submit(new FormData(e.target as HTMLFormElement));
         }}
-        className={'flex flex-col space-y-2 w-40 mx-auto'}
+        className="flex flex-col space-y-4 p-8 bg-area-dark rounded-lg shadow-card w-80 mx-auto border-[1.5px] border-stroke-gray-dark"
       >
-        <h1>Login</h1>
+        <h1 className="text-2xl font-bold text-center mb-4 text-white font-inter">
+          Login
+        </h1>
+
         <input
-          name={'username'}
+          name="username"
           placeholder="Username"
-          className="py-1 text-neutral-950 dark:text-neutral-950"
+          className="px-4 py-2 border-[1.5px] border-stroke-gray-dark rounded-lg 
+                focus:outline-none focus:ring-2 focus:ring-blue-500
+                bg-area-dark text-white placeholder-gray
+                transition-colors duration-200"
         />
+
         <input
-          name={'password'}
+          name="password"
           placeholder="Password"
           type="password"
-          className="py-1 text-neutral-950 dark:text-neutral-950"
+
+          className="px-4 py-2 border-[1.5px] border-stroke-gray-dark rounded-lg 
+                focus:outline-none focus:ring-2 focus:ring-blue-500
+                bg-area-dark text-white placeholder-gray
+                transition-colors duration-200"
         />
-        <button type="submit" className="py-1">
+
+        <button
+          type="submit"
+          className="mt-4 px-4 py-2 bg-blue ring-2 ring-blue hover:bg-area text-white 
+            rounded-lg transition-all duration-200 font-medium font-inter
+            focus:outline-none focus:ring-2 focus:ring-blue focus:ring-offset-2 
+            focus:ring-offset-area-dark"
+        >
           Login
         </button>
       </form>
-      <button
-        onClick={event => {
-          event.preventDefault();
-          checkLogin();
-        }}
-      >
-        Check login
-      </button>
+      <div className="mt-4 text-center">
+        <button
+          onClick={event => {
+            event.preventDefault();
+            checkLogin();
+          }}
+          className="px-4 py-2 border-2 border-blue text-blue hover:bg-stroke-blue/20
+                rounded-lg transition-colors duration-200 font-inter
+                focus:outline-none focus:ring-2 focus:ring-blue-500"
+        >
+          Check login
+        </button>
+      </div>
     </div>
   );
 };
