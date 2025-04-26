35ef904ac8f93fa745e7b4dbd878416b9e4acb67
Fa4stik <stepan.sharifulin@mail.ru>
2025-04-26 13:58:35 +0500

    feat: - deploy front

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom'; // ?? react-router
 import { Header, Sidebar } from './components';
 
 export const Layout = () => {
