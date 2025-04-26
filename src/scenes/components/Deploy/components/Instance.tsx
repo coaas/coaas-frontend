7a8d61b95e1eae5ae85723e4977dfa0a9e76db35
Fa4stik <stepan.sharifulin@mail.ru>
2025-04-26 13:57:39 +0500

    feat: - Refactor page for clusters type 0

---
+++ src/scenes/components/Deploy/components/Instance.tsx
@@ -0,0 +1,42 @@
+import { Status } from '@scenes/components/Deploy/model/types.ts';
+import { clsx } from 'clsx';
+import type { Instance as InstanceType } from '../model/types.ts';
+
+export const Instance = ({
+  status,
+  cpu,
+  ram,
+  service: { name },
+}: InstanceType) => {
+  const variants = {
+    color: {
+      [Status.ACTIVE]: 'bg-green/20 border-green',
+      [Status.LOADED]: 'bg-orange/20 border-orange',
+      [Status.DIED]: 'bg-error/20 border-error',
+      [Status.UNKNOWN]: 'bg-gray/20 border-gray',
+    },
+    text: {
+      [Status.ACTIVE]: 'text-green',
+      [Status.LOADED]: 'text-orange',
+      [Status.DIED]: 'text-error',
+      [Status.UNKNOWN]: 'text-gray',
+    },
+  };
+
+  return (
+    <div
+      className={clsx(
+        'w-[334px] h-[52px] py-4 px-6 rounded-lg border',
+        variants.color[status],
+      )}
+    >
+      <div className="flex justify-between items-center">
+        <p className={variants.text[status]}>{name}</p>
+        <div className="flex gap-2 text-sm">
+          <p>CPU: {cpu}</p>
+          <p>RAM: {ram}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
