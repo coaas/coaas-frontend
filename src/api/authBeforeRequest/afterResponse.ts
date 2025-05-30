d132773738ce42637abb1cb210a599aa3c300701
Fa4stik <stepan.sharifulin@mail.ru>
2025-05-30 22:19:09 +0500

    fix: - update logic for refresh

--- src/api/authBeforeRequest/afterResponse.ts
+++ src/api/authBeforeRequest/afterResponse.ts
@@ -12,7 +12,10 @@ export const afterResponse: AfterResponseHook = async (
   response,
 ) => {
   if (
-    [401, 404].includes(response.status) ||
+    ([401, 404].includes(response.status) &&
+      ['Session not found', 'Token expired'].includes(
+        (await response.json<Record<string, string>>()).value,
+      )) ||
     ENDPOINTS_TO_REFRESH.some(e => response.url.includes(e))
   ) {
     await obtainAccess();
