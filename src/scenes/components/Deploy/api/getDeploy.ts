aa40e4a3d4574c932ed45ea8f34a1390405c3ebd
Степан <104088567+Fa4stik@users.noreply.github.com>
2025-06-01 17:16:47 +0500

    fix: (#52)
    - add deploy if doesnt exist

--- src/scenes/components/Deploy/api/getDeploy.ts
+++ src/scenes/components/Deploy/api/getDeploy.ts
@@ -3,7 +3,9 @@ import { queryOptions } from '@tanstack/react-query';
 import { api, IS_MOCK_ACTIVE } from '@api/constants.ts';
 
 import clusters from './mock-cluster.json';
-import { Cluster } from '../model/cluster.types.ts';
+import { Cluster, OrchEngine } from '../model/cluster.types.ts';
+import { Success, successMock } from '@scenes/components/Deploy/api/common.ts';
+import { ServerType } from '@scenes/components/Deploy/model/dataCenter.types.ts';
 
 const DEPLOY_ENDPOINT = 'DeployService/GetProjectDeploy';
 export type ClustersResponse = {
@@ -22,3 +24,15 @@ export const clusterOptions = queryOptions({
   queryFn: getDeploy,
   queryKey: ['clusters'],
 });
+
+const DEPLOY_ENDPOINT_ADD = 'DeployService/AddProjectDeploy';
+export type AddProjectDeployRequest = {
+  type: ServerType;
+  orchestration_engine: OrchEngine;
+};
+export const addProjectDeploy = (body: AddProjectDeployRequest) =>
+  IS_MOCK_ACTIVE
+    ? successMock(`Project deploy added successfully, ${JSON.stringify(body)}`)
+    : api
+        .post<Success>(DEPLOY_ENDPOINT_ADD, { body: JSON.stringify(body) })
+        .json();
---
+++ src/scenes/components/Deploy/components/NotDeployed.tsx
@@ -0,0 +1,107 @@
+import { ServerType } from '@scenes/components/Deploy/model/dataCenter.types.ts';
+import { OrchEngine } from '@scenes/components/Deploy/model/cluster.types.ts';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@components/Select/Select.tsx';
+import { DeployButton } from '@scenes/components/Deploy/components/Common/DeployButton.tsx';
+import { useState } from 'react';
+import {
+  addProjectDeploy,
+  AddProjectDeployRequest,
+  clusterOptions,
+} from '@scenes/components/Deploy/api/getDeploy.ts';
+import { useMutation } from '@tanstack/react-query';
+import { queryClient } from '@api/constants.ts';
+
+export const NotDeployed = () => {
+  const [addDeployInput, setAddDeployInput] =
+    useState<Partial<AddProjectDeployRequest> | null>(null);
+
+  const { mutate: mutateAddProject } = useMutation({
+    mutationFn: addProjectDeploy,
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({
+        queryKey: clusterOptions.queryKey,
+      });
+    },
+  });
+
+  return (
+    <div>
+      <p className="text-xl text-center mt-8">
+        You have not deployed project yet.
+      </p>
+      <form
+        onSubmit={e => e.preventDefault()}
+        className="mx-40 flex flex-col gap-4 mt-8"
+      >
+        {(
+          [
+            {
+              id: 'type',
+              placeholder: 'Select type of the deploy...',
+              options: [
+                { value: ServerType.SHARED, label: 'Shared' },
+                { value: ServerType.PERSONAL, label: 'Personal' },
+                { value: ServerType.PURCHASED, label: 'Purchased' },
+              ],
+            },
+            {
+              id: 'orchestration_engine',
+              placeholder: 'Select type of orchestration...',
+              options: [
+                { value: OrchEngine.DOCKER, label: 'Docker' },
+                { value: OrchEngine.KUBERNETES, label: 'Kubernetes' },
+              ],
+            },
+          ] as const
+        ).map(({ id, options, placeholder }) => (
+          <Select
+            onValueChange={value =>
+              setAddDeployInput(prevState =>
+                prevState
+                  ? {
+                      ...prevState,
+                      [id]: Number(value),
+                    }
+                  : {
+                      [id]: Number(value),
+                    },
+              )
+            }
+            key={id}
+          >
+            <SelectTrigger>
+              <SelectValue placeholder={placeholder} />
+            </SelectTrigger>
+            <SelectContent>
+              {options.map(({ value, label }) => (
+                <SelectItem key={`${id}-${value}`} value={String(value)}>
+                  {label}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        ))}
+        <DeployButton
+          color={'area'}
+          onClick={() => {
+            if (
+              !addDeployInput ||
+              !addDeployInput.type ||
+              !addDeployInput.orchestration_engine
+            )
+              return;
+            mutateAddProject(addDeployInput as AddProjectDeployRequest);
+          }}
+        >
+          Deploy
+        </DeployButton>
+      </form>
+    </div>
+  );
+};
